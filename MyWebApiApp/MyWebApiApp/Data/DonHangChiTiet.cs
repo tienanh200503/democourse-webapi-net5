a10c3e12b94e1552a35707ed427abea516977ee1
leetienks1 <phamanhvs20@gmail.com>
2024-05-15 23:30:23 +0700

    update 1

---
+++ MyWebApiApp/MyWebApiApp/Data/DonHangChiTiet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyWebApiApp.Data
+{
+    public class DonHangChiTiet
+    {
+        public Guid MaHh { get; set; }
+        public Guid MaDh { get; set; }
+        public int SoLuong { get; set; }
+        public double DonGia { get; set; }
+        public byte GiamGia { get; set; }
+
+        public DonHang DonHang { get; set; }
+        public HangHoa HangHoa { get; set; }
+    }
+}
---
+++ MyWebApiApp/MyWebApiApp/Data/HangHoa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyWebApiApp.Data
+{
+    [Table("HangHoa")]
+    public class HangHoa
+    {
+        [Key]
+        public Guid MaHh { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string TenHh { get; set; } 
+        public string MoTa { get; set; }
+        [Range(0, double.MaxValue)]
+        public double DonGia { get; set; }
+        public byte GiamGia { get; set; }
+
+        public int? maLoai { get; set; }
+        [ForeignKey("maLoai")]
+        public Loai Loai { get; set; }
+
+        public ICollection<DonHangChiTiet> DonHangChiTiets { get; set; }
+
+        public HangHoa()
+        {
+            DonHangChiTiets = new List<DonHangChiTiet>();
+        }
+
+    }
+}
